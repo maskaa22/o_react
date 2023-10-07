3de36211ceac5a1c47d3586fdf7eebb55b33b80f
maskaa22 <kyrosika565@gmail.com>
2023-10-07 16:41:40 +0300

    admin page log

--- src/components/admin/AdminHomePage.js
+++ src/components/admin/AdminHomePage.js
@@ -40,15 +40,16 @@ export function AdminHomePage({setUsers, setDelUser, delUser}) {
     useEffect(() => {
         if (localStorage.getItem(WORD_TOKEN)) {
             dispatch(auth()).then(req => {
-                if (req === undefined) {
-                    navigate(LOGIN);
-                }
-                getUsers().then(response => {
-                    if (response !== undefined) {
-                        setUsers(response.data);
-                    }
-                });
-                setDelUser(false);
+                console.log(req);
+                // if (req === undefined) {
+                //     navigate(LOGIN);
+                // }
+                // getUsers().then(response => {
+                //     if (response !== undefined) {
+                //         setUsers(response.data);
+                //     }
+                // });
+                // setDelUser(false);
             })
         }
     }, [dispatch, navigate, setUsers, delUser, setDelUser]);
